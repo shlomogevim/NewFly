4e764d910df870f13674a8927d991017d637c1c2
shlomo <shlomo.gevim@gmail.com>
2021-10-17 07:21:55 +0300

    Initial commit

---
+++ app/src/main/java/com/sg/newfly/MainActivity.kt
@@ -0,0 +1,201 @@
+package com.sg.newfly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.ar.core.Anchor
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.animation.ModelAnimator
+import com.google.ar.sceneform.math.Quaternion
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var arFragment: ArFragment
+    private val nodes = mutableListOf<RotatingNode>()
+    private val model = Models.Bee
+    private val modelResourceId=R.raw.beedrill
+    private var curCameraPosition=Vector3.zero()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        arFragment=fragment as ArFragment
+        arFragment.setOnTapArPlaneListener { hitResult, _, _ ->
+            loadModelAndAddToSence(hitResult.createAnchor(),modelResourceId)
+        }
+    }
+
+    private fun loadModelAndAddToSence(anchor: Anchor?, modelResourceId: Int) {
+        ModelRenderable.builder()
+            .setSource(this,modelResourceId)
+            .build()
+            .thenAccept { modelRenderable->
+                addNodeToScene(anchor,modelRenderable)
+                eliminateDot()
+            }.exceptionally {
+                Toast.makeText(this,"Error creatind nodes:$it",Toast.LENGTH_LONG).show()
+                null
+            }
+
+    }
+
+    private fun eliminateDot() {
+        arFragment.arSceneView.planeRenderer.isVisible = false
+        arFragment.planeDiscoveryController.hide()
+        arFragment.planeDiscoveryController.setInstructionView(null)
+    }
+    private fun addNodeToScene(
+        anchor: Anchor?,
+        modelRenderable: ModelRenderable?
+    ) {
+        val anchorNode=AnchorNode(anchor)
+        val rotatingNode=RotatingNode(model.degreesPerSecond).apply {
+            setParent(anchorNode)
+        }
+        Node().apply {
+            renderable=modelRenderable
+            setParent(rotatingNode)
+            localPosition= Vector3(model.radius,model.height,0f)
+            localRotation= Quaternion.eulerAngles(Vector3(0f,model.rotationDegrees,0f))
+        }
+      arFragment.arSceneView.scene.addChild(anchorNode)
+      nodes.add(rotatingNode)
+      val animationData=modelRenderable?.getAnimationData("Beedrill_Animation")
+      ModelAnimator(animationData,modelRenderable).apply {
+          repeatCount=ModelAnimator.INFINITE
+          start()
+      }
+    }
+
+
+
+
+}
+
+//Original versia of fly
+/*  private val spaceship = Models.Bee
+  private var modelResourceId = R.raw.beedrill
+
+  private val nodes = mutableListOf<RotatingNode>()
+  private lateinit var arFragment: ArFragment
+  private var curCameraPosition = Vector3.zero()
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.activity_main)
+      arFragment = fragment as ArFragment
+      arFragment.setOnTapArPlaneListener { hitResult, _, _ ->
+          loadModelAndAddToScene(hitResult.createAnchor(), modelResourceId)
+      }
+      arFragment.arSceneView.scene.addOnUpdateListener {
+          updateNodes()
+      }
+      setupFab()
+  }
+
+  private fun loadModelAndAddToScene(anchor: Anchor, modelResourceId: Int) {
+      ModelRenderable.builder()
+          .setSource(this, modelResourceId)
+          .build()
+          .thenAccept { modelRenderable ->
+              addNodeToScene(anchor, modelRenderable, spaceship)
+              eliminateDot()
+          }.exceptionally {
+              Toast.makeText(this, "Error creating node: $it", Toast.LENGTH_LONG).show()
+              null
+          }
+
+  }
+
+  private fun updateNodes() {
+      curCameraPosition = arFragment.arSceneView.scene.camera.worldPosition
+      for (node in nodes) {
+          node.worldPosition =
+              Vector3(curCameraPosition.x, node.worldPosition.y, curCameraPosition.z)
+      }
+  }
+
+  private fun addNodeToScene(
+      anchor: Anchor,
+      modelRenderable: ModelRenderable,
+      spaceship: Models
+  ) {
+      val anchorNode = AnchorNode(anchor)
+      val rotatingNode = RotatingNode(spaceship.degreesPerSecond).apply {
+          setParent(anchorNode)
+      }
+      Node().apply {
+          renderable = modelRenderable
+          setParent(rotatingNode)
+          localPosition = Vector3(spaceship.radius, spaceship.height, 0f)
+          localRotation = Quaternion.eulerAngles(Vector3(0f, spaceship.rotationDegrees, 0f))
+      }
+      arFragment.arSceneView.scene.addChild(anchorNode)
+      nodes.add(rotatingNode)
+      val animationData = modelRenderable.getAnimationData("Beedrill_Animation")
+      ModelAnimator(animationData, modelRenderable).apply {
+          repeatCount = ModelAnimator.INFINITE
+          start()
+      }
+
+  }
+
+  private fun addNodeToScene(anchor: Anchor, modelRenderable: ModelRenderable) {
+      val anchorNode = AnchorNode(anchor)
+      TransformableNode(arFragment.transformationSystem).apply {
+          renderable = modelRenderable
+          setParent(anchorNode)
+      }
+      arFragment.arSceneView.scene.addChild(anchorNode)
+  }
+
+  private fun setupFab() {
+      *//*      photoSaver = PhotoSaver(this)
+              videoRecorder = VideoRecorder(this).apply {
+                  sceneView = arFragment.arSceneView
+
+                  setVideoQuality(CamcorderProfile.QUALITY_1080P, resources.configuration.orientation)
+              }
+              fab.setOnClickListener {
+                  if (!isRecording) {
+                      photoSaver.takePhoto(arFragment.arSceneView)
+                  }
+              }
+
+              fab.setOnLongClickListener {
+                  isRecording = videoRecorder.toggleRecordingState()
+                  true
+              }
+
+              fab.setOnTouchListener { view, motionEvent ->
+
+                  if (motionEvent.action == MotionEvent.ACTION_UP && isRecording) {
+
+                      isRecording = videoRecorder.toggleRecordingState()
+
+                      Toast.makeText(this, "Saved video to gallery!", Toast.LENGTH_LONG).show()
+
+                      true
+
+                  } else false
+
+              }*//*
+
+    }
+
+
+
+private fun eliminateDot() {
+    arFragment.arSceneView.planeRenderer.isVisible = false
+    arFragment.planeDiscoveryController.hide()
+    arFragment.planeDiscoveryController.setInstructionView(null)
+}
+}
+
+        */
---
+++ app/src/main/java/com/sg/newfly/Models.kt
@@ -0,0 +1,63 @@
+package com.sg.newfly
+
+sealed class Models {
+
+    abstract val degreesPerSecond: Float
+    abstract val radius: Float
+    abstract val height: Float
+    abstract val rotationDegrees: Float
+
+    object Fly : Models() {
+        override val degreesPerSecond: Float
+            get() = 20f
+        override val radius: Float
+            get() = 2f
+        override val height: Float
+            get() = 0.7f
+        override val rotationDegrees: Float
+            get() = 180f
+    }
+    object Bird : Models() {
+        override val degreesPerSecond: Float
+            get() = 20f
+        override val radius: Float
+            get() = 2f
+        override val height: Float
+            get() = 0.7f
+        override val rotationDegrees: Float
+            get() = 180f
+    }
+    object Fish : Models() {
+        override val degreesPerSecond: Float
+            get() = 20f
+        override val radius: Float
+            get() = 2f
+        override val height: Float
+            get() = 0.7f
+        override val rotationDegrees: Float
+            get() = 180f
+    }
+
+    object Bee : Models() {
+        override val degreesPerSecond: Float
+            get() = 10f
+        override val radius: Float
+            get() = 2f
+        override val height: Float
+            get() = 1.0f
+        override val rotationDegrees: Float
+            get() = 180f
+    }
+    object Jet : Models() {
+        override val degreesPerSecond: Float
+            get() = 20f
+        override val radius: Float
+            get() = 2f
+        override val height: Float
+            get() = 0.7f
+        override val rotationDegrees: Float
+            get() = 180f
+    }
+
+
+}
